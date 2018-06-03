2c5e527f84a9c6cbfe839fe0c53b7abcca4410b0
valvesss <vitoralvesinfo@gmail.com>
2018-06-02 23:33:12 -0300

    Add files via upload

--- coinconsult.py
+++ coinconsult.py
@@ -13,7 +13,7 @@ def coinstats():
         url.append("https://yobit.net/api/3/ticker/btc_usd")
     else:
         url.append("https://api.kraken.com/0/public/Ticker?pair=%sUSD" % currency)
-        url.append("https://yobit.net/api/3/ticker/%s_btc" % currency)
+        url.append("https://yobit.net/api/3/ticker/%s_usd" % currency)
     url.append("https://www.okcoin.com/api/v1/ticker.do?symbol=%s_usd" % currency)
     url.append("https://bittrex.com/api/v1.1/public/getticker?market=usdt-%s" % currency)
     webconsult(currency,url)
@@ -40,13 +40,14 @@ def webconsult(currency,url):
             if (currency == 'btc') or (currency == 'BTC'):
                 field = 'btc_usd'
             else:
-                field = currency + '_btc'
+                field = currency + '_usd'
+                print(field)
             bitcoinvalue = float(response[field]['last'])
             exchange = "yobit"
         matrix[i][0] = exchange
         matrix[i][1] = bitcoinvalue
     matrix.sort(key=lambda x:x[1],reverse=True)
-    print("\nPreço do %s:\n" % currency.upper())
+    print("\nPreço do %s em USD:\n" % currency.upper())
     for i in range(len(matrix)):
         print(matrix[i][0].title() + ":  \t",round(float(matrix[i][1]),8))
     print("")
